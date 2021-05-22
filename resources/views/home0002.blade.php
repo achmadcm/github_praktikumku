f1b649aa397f5cdcef3944163e1831aa58092583
achmadcm <achmadcm.cm@gmail.com>
2021-05-23 06:43:08 +0700

    home0002

---
+++ resources/views/home0002.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+  //include 'connection.php';
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NBI 1461900002 #1 | ACHMAD CHUSNI MUBAROK</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <header class="header">
+    <h1> NBI 1461900002</h1>
+    <p>Kegiatan Praktikum 1</p>
+    <p>membuat halaman web yang terdiri dari 3 menu</p>
+  </header>
+
+  <nav class="navbar">
+    <ul>
+      <li class="utama"><a href="/web">Home</a></li>
+      <li class="utama"><a href="index.php?web=form">Artikel</a></li>
+      <li class="utama"><a href="#">Contact Us</a>
+        <ul>
+          <li><a href="">Biodata</a></li>
+          <li><a href="">My Photo</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+
+  <main class="main">
+
+    <div class="content">
+      <h2>TITLE 1</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio incidunt at doloremque reiciendis ea voluptas
+        placeat architecto sequi! Corporis, doloribus, vero natus non magnam eligendi quisquam accusantium placeat
+        tempora in provident! Possimus hic aperiam quo, modi dolorem et voluptatum, numquam, esse libero autem suscipit.
+        Suscipit veniam iure magni doloremque numquam.</p>
+      <br>
+      <h2>TITLE 2</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio incidunt at doloremque reiciendis ea voluptas
+        placeat architecto sequi! Corporis, doloribus, vero natus non magnam eligendi quisquam accusantium placeat
+        tempora in provident! Possimus hic aperiam quo, modi dolorem et voluptatum, numquam, esse libero autem suscipit.
+        Suscipit veniam iure magni doloremque numquam.</p>
+    </div>
+  </main>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home0002', function () {
+    return view('home0002');
 });
